ddb055e11be78036b004fea7efbcc7a248699db2
Rajat012 <80401767+Rajat012@users.noreply.github.com>
2021-03-15 19:53:03 +0530

    Create Pyramid.py

---
+++ Pyramid.py
@@ -0,0 +1,41 @@
+def noOfRows(x):
+    for i in range(1,x+1):
+        print('* '*i)
+        
+a=int(input("Enter No. of Rows  :  "))
+noOfRows(a)
+
+'''
+# For Odd Series
+def noOfRows(x):
+    for i in range(1,x+1):
+        print('* '*((2*i)-1))
+        
+a=int(input("Enter No. of Rows  :  "))
+noOfRows(a)
+
+#For Even Series
+def noOfRows(x):
+    for i in range(1,x+1):
+        print('* '*(2*i))
+        
+a=int(input("Enter No. of Rows  :  "))
+noOfRows(a)
+
+# For Pyramid
+def noOfRows(x):
+    for i in range(1,x+1):
+        print(" "*(x-i)+'*'*((2*i)-1))
+        
+a=int(input("Enter No. of Rows  :  "))
+noOfRows(a)
+
+# Starts From Right
+def noOfRows(x):
+    for i in range(1,x+1):
+        print(" "*(x-i)+'*'*i)
+        
+a=int(input("Enter No. of Rows  :  "))
+noOfRows(a)
+'''
+

a8d170ad244e41050b6eab4970ea32f900106ff2
Rajat012 <80401767+Rajat012@users.noreply.github.com>
2021-03-19 20:09:20 +0530

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -22,3 +22,27 @@ list1=[10,20,30,40,50,60,70]
 list1.sort()
 ind=binarySearch(list1,10)
 if (ind!=None):print(ind)
+  
+  
+# Method 2
+def binarySearch(list1,key):
+  low=0
+  high=len(list1)-1
+
+  while(low<=high):
+    mid= (low+high)//2
+    if(key==list1[mid]):
+      return mid
+    if(key>list1[mid]):
+      low=mid+1
+    if(key<list1[mid]):
+      high=mid-1  
+
+      
+list1=[10,20,30,40,50,60,70]
+list1.sort()
+key=int(input("Enter Key : "))
+ind=binarySearch(list1,key)
+if ind!=None:print(ind)
+else:print("Key Not Found")  
+  
